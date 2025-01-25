17e394f410c7b256b1676b4c413e0d47f1b0a26f
Achilleas Mitos <axmito@gmail.com>
2025-01-25 13:49:40 +0200

    Changed existing comments to Doxygen comments
    With the support of the VSDoxyHighlighter extension, the conventional
    way of writing Doxygen comments changed. We adapt existing comments
    to this new way.

--- Source/DataStructures/LinkedList/DoublyLinkedList.h
+++ Source/DataStructures/LinkedList/DoublyLinkedList.h
@@ -4,11 +4,9 @@ namespace datastructures
 {
 #pragma warning(push)
 #pragma warning(disable : 4820) // Depending on the value of TValue here, a padding error may appear in VS compiler
-/// <summary>
-/// This represents a node in a doubly linked list.
+/// @brief This represents a node in a doubly linked list.
 /// It contains a value as well as a pointer to its next element and a pointer to its previous element (nullptr by default).
-/// </summary>
-/// <typeparam name="TValue">The value stored in the node, for example 'int' or 'std::string'</typeparam>
+/// @tparam TValue The value stored in the node, for example 'int' or 'std::string'
 template<typename TValue>
 struct DLListNode
 {
@@ -18,15 +16,13 @@ struct DLListNode
 };
 #pragma warning(pop)
 
-/// <summary>
-/// A doubly linked list is the most common alternative type of a linked list (after a singly linked list).
+/// @brief A doubly linked list is the most common alternative type of a linked list (after a singly linked list).
 /// Each of its nodes points forward to the next node and backwards to its previous element.
 ///
 /// The list is just a wrapper around its first (head) node. This can be a nullptr,
-/// indicating an empty list.
-/// A node that forward-points to nullptr is considered to be the last node of the list.
-/// </summary>
-/// <typeparam name="TValue">The value stored in each of the list's nodes.</typeparam>
+/// indicating an empty list. Some doubly linked lists also provide a pointer to
+/// their last element, allowing for easy backwards traversal of the list (see Queues, for example)
+/// @tparam TValue The value stored in each of the list's nodes.
 template<typename TValue>
 struct DoublyLinkedList
 {
--- Source/DataStructures/LinkedList/SinglyLinkedList.h
+++ Source/DataStructures/LinkedList/SinglyLinkedList.h
@@ -4,11 +4,9 @@ namespace datastructures
 {
 #pragma warning(push)
 #pragma warning(disable : 4820) // Depending on the value of TValue here, a padding error may appear in VS compiler
-/// <summary>
-/// This represents a node in a singly linked list.
+/// @brief This represents a node in a singly linked list.
 /// It contains a value as well as a pointer to its next element (nullptr by default).
-/// </summary>
-/// <typeparam name="TValue">The value stored in the node, for example 'int' or 'std::string'</typeparam>
+/// @tparam TValue The value stored in the node, for example 'int' or 'std::string'
 template<typename TValue>
 struct SLListNode
 {
@@ -17,15 +15,13 @@ struct SLListNode
 };
 #pragma warning(pop)
 
-/// <summary>
-/// A singly linked list is the most common type of a linked list.
+/// @brief A singly linked list is the most common type of a linked list.
 /// Each of its nodes only points forward to the next node.
 ///
 /// The list is just a wrapper around its first (head) node. This can be a nullptr,
 /// indicating an empty list.
 /// A node that points to nullptr is considered to be the last node of the list.
-/// </summary>
-/// <typeparam name="TValue">The value stored in each of the list's nodes.</typeparam>
+/// @tparam TValue The value stored in each of the list's nodes.
 template<typename TValue>
 struct SinglyLinkedList
 {
